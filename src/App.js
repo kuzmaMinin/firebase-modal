d07712a756e2ffe6a1a6c3adc6cecef953f640e5
sombra-industrial <sombra-industrial@yandex.ru>
2020-10-17 22:08:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            hasAccount: false,
+            name: '',
+            key: '',
+            value: '',
+        }
+    }
+
+    handleChange = ({target: {value, id}}) => {
+        this.setState({
+            [id]: value,
+        })
+    }
+
+    componentDidMount() {
+        const db = firebase.database()
+        const name = db.ref('name')
+        name.on('value', (elem) => {
+            this.setState({name: elem.val()})
+        })
+    }
+
+    createAccount = () => {
+        const {email, password} = this.state
+        // firebase.auth().createUserWithEmailAndPassword(email, password)
+        //    .catch(error => console.log(error))
+
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(response => {
+                this.setState({hasAccount: true})
+            })
+            .catch(error => console.log(error))
+
+        this.setState({
+            email: '',
+            password: ''
+        })
+    }
+
+    sendData = () => {
+        const {key, value} = this.state
+        const db = firebase.database()
+        db.ref(key).push(value)
+        alert('ваши данные были добавлены в базу данных, вась')
+        this.setState({
+            key: '',
+            value: ''
+        })
+    }
+
+    render() {
+        const {hasAccount, name} = this.state
+        return (
+            <div>
+                {
+                    hasAccount ?
+                        (
+                            <div className='add_block'>
+                                <input
+                                    type="text"id='key'
+                                    placeholder='введите ключ'
+                                    onChange={this.handleChange}
+                                    value={this.state.key}
+                                />
+                                <input
+                                    type="text"
+                                    id='value'
+                                    placeholder='введите значение'
+                                    onChange={this.handleChange}
+                                    value={this.state.value}
+                                />
+                                <input
+                                    type="submit"
+                                    onClick={this.sendData}
+                                    value='добавить в БД'
+                                />
+                            </div>
+                        )
+                        :
+                        (
+                            <div className='login_block'>
+                                <input
+                                    type="text" id='email'
+                                    placeholder='email'
+                                    onChange={this.handleChange}
+                                    value={this.state.email}
+                                />
+                                <input
+                                    type="password"
+                                    id='password'
+                                    placeholder='password'
+                                    onChange={this.handleChange}
+                                    value={this.state.password}
+                                />
+                                <input
+                                    type="submit"
+                                    onClick={this.createAccount}
+
+                                />
+                            </div>
+                        )
+                }
+
+            </div>
+        )
+    }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBcCSWbNQUoyx6qO3qWLowPd7gks5RLKo0",
+    authDomain: "fir-test-project-2a128.firebaseapp.com",
+    databaseURL: "https://fir-test-project-2a128.firebaseio.com",
+    projectId: "fir-test-project-2a128",
+    storageBucket: "fir-test-project-2a128.appspot.com",
+    messagingSenderId: "501814001255",
+    appId: "1:501814001255:web:0c95bd0019ceda1da25ec2"
+}
+
+firebase.initializeApp(firebaseConfig)
 ReactDOM.render(
   <React.StrictMode>
     <App />
